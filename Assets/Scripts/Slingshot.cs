d8004c897b4ca29c7d99b382ec48d1a1f6365e2c
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-09-22 11:12:10 -0400

    now the slingshot glows

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+
+    public GameObject launchPoint;
+
+
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+
+        launchPoint = launchPointTrans.gameObject;
+
+        launchPoint.SetActive(false);
+    }
+
+
+    private void OnMouseEnter()
+    {
+        //print("slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        //print("slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+
+}
